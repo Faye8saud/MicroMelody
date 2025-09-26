8a45f0a1f7f117c1135e43a372d0806401dbebe3
fay <fayii8822@gmail.com>
2025-09-26 22:05:46 +0300

    Initial Commit

---
+++ MicroMelody/MicroMelodyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicroMelodyApp.swift
+//  MicroMelody
+//
+//  Created by Fay  on 26/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct MicroMelodyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MicroMelodyTests/MicroMelodyTests.swift
@@ -0,0 +1,17 @@
+//
+//  MicroMelodyTests.swift
+//  MicroMelodyTests
+//
+//  Created by Fay  on 26/09/2025.
+//
+
+import Testing
+@testable import MicroMelody
+
+struct MicroMelodyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
